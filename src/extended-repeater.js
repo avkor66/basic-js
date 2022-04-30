0160eef9d23aa57021f778cdc3bf701636829403
Artem Korotkov <avkor66@gmail.com>
2022-05-01 00:23:34 +0300

    feat: complete extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = str + ''
+  if (options.hasOwnProperty('addition')) options.addition = options.addition + ''
+  let temp = []
+  if (options.repeatTimes) {
+    for (let index = 0; index < options.repeatTimes-1; index++) {
+      temp.push(str)
+      if (options.addition) {
+        if(options.additionRepeatTimes) {
+          for (let i = 0; i < options.additionRepeatTimes-1; i++) {
+            temp.push(options.addition)
+            temp.push(options.additionSeparator ?? '|')
+          }
+          temp.push(options.addition)
+        } else {
+          temp.push(options.addition)
+        }
+      } 
+      temp.push(options.separator ?? '+')
+    }
+    temp.push(str)
+    if (options.addition) {
+      if(options.additionRepeatTimes) {
+        for (let i = 0; i < options.additionRepeatTimes-1; i++) {
+          temp.push(options.addition)
+          temp.push(options.additionSeparator ?? '|')
+        }
+        temp.push(options.addition)
+      } else {
+        temp.push(options.addition)
+      }
+    } 
+  } else {
+    temp.push(str)
+    if (options.addition) {
+      if(options.additionRepeatTimes) {
+        for (let i = 0; i < options.additionRepeatTimes-1; i++) {
+          temp.push(options.addition)
+          temp.push(options.additionSeparator ?? '|')
+        }
+        temp.push(options.addition)
+      } else {
+        temp.push(options.addition)
+      }
+    }
+  }
+
+  return temp.join('')
 }
 
 module.exports = {
